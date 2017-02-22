015b1fb69c68d951c261c1d1007a43684f0ed891
Gilbert Brault <gilbert.brault@hotmail.com>
2017-02-22 10:07:14 +0100

    added size

--- jscode.js
+++ jscode.js
@@ -58,6 +58,58 @@ for(var rows=0; rows<marimekko.rows.length; rows++){
           	title=marimekko.rows[rows].title.substr(0,3);
         }
     }
+  title += Math.round(marimekko.rows[rows].size*10)/10;
+  slide.addText(title, { shape:pptx.shapes.RECTANGLE, 
+                                             x:marimekko.rows[rows].box.Left/scalex/inch, 
+                                             y:marimekko.rows[rows].box.Top/scaley/inch, 
+                                             w:wcm/inch, 
+                                             h:hcm/inch,
+                                             fill:'FFFFFF',
+											 line: '5B9BD5',
+                                             align:'c',
+                                             font_size:fs });
+  for(var cols=0; cols<marimekko.rows[rows].cols.length; cols++){
+        var fs=11,title,wcm,hcm;
+        wcm = marimekko.rows[rows].cols[cols].box.Width/scalex;
+        hcm=marimekko.rows[rows].cols[cols].box.Height/scaley;
+        if(hcm<0.5){
+          	fs=4;
+        } else if(hcm<1){
+          fs=6;
+        } else if(hcm<1.5){
+          fs=8;
+        }
+    	title=marimekko.rows[rows].cols[cols].title;
+        if(wcm<2.5){
+          	title=marimekko.shortTitleCols[title];
+            if(title==undefined){
+              	title=marimekko.rows[rows].cols[cols].title.substr(0,3);
+            }
+        }
+    	title += Math.round(marimekko.rows[rows].cols[cols].size)+"%";
+  		slide.addText(title, { shape:pptx.shapes.RECTANGLE, 
+                                             		x:((marimekko.rows[rows].cols[cols].box.Left/scalex)-offsetx)/inch, 
+                                             		y:marimekko.rows[rows].cols[cols].box.Top/scaley/inch, 
+                                             		w:wcm/inch, 
+                                             		h:hcm/inch,
+                                             		fill:'FFFFFF',
+											 		line: '5B9BD5',
+                                             		align:'c',
+                                             		font_size:fs });    
+  }
+}
+
+pptx.save('Marimekko'+'_'+getTimestamp());
+    } else if(hcm<1.5){
+       fs=8;
+    }
+    title=marimekko.rows[rows].title;
+    if(wcm<2.5){
+       	title=marimekko.shortTitleRows[title];
+        if(title==undefined){
+          	title=marimekko.rows[rows].title.substr(0,3);
+        }
+    }
   slide.addText(title, { shape:pptx.shapes.RECTANGLE, 
                                              x:marimekko.rows[rows].box.Left/scalex/inch, 
                                              y:marimekko.rows[rows].box.Top/scaley/inch, 
