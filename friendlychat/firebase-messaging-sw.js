fd59247b005aca8ca43a73d309416e97aa75d656
Gilbert Brault <gilbert.brault@hotmail.com>
2017-03-17 14:20:01 +0100

    Update firebase-messaging-sw.js

--- friendlychat/firebase-messaging-sw.js
+++ friendlychat/firebase-messaging-sw.js
@@ -4,6 +4,6 @@ importScripts('https://www.gstatic.com/firebasejs/3.6.6/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-  messagingSenderId: '670781077200'
+  messagingSenderId: '564501243524'
 });
 var messaging = firebase.messaging();
