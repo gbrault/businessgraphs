7ab92f50c7bbc6ee7dfc213a208ddf85c94f4c04
Gilbert Brault <gilbert.brault@hotmail.com>
2017-03-17 11:38:23 +0100

    Add files via upload

---
+++ firebase/config.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var config = {
+    apiKey: "AIzaSyBZJLQp5xaL0tdFvUsx3I97kfweCAQ2C4w",
+    authDomain: "businessgraphs-27585.firebaseapp.com",
+    databaseURL: "https://businessgraphs-27585.firebaseio.com",
+    storageBucket: "businessgraphs-27585.appspot.com",
+    messagingSenderId: "441454731465"
+};
+firebase.initializeApp(config);
